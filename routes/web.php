1a69364632ded29c7822a945ddfc62bb632f0024
vinzencor <vinzenco44a@gmail.com>
2024-05-02 12:41:07 +0530

    firstphp

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+
+// we can shorten the code
+
+Route :: view('/','home');
